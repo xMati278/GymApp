c58a495d46ae59f8ef0823c1aced06874b2f2a28
xMati278 <59803172+xMati278@users.noreply.github.com>
2023-12-20 13:32:34 +0100

    feat: added create, delete exercise path and get usertraining plans path

--- API/urls.py
+++ API/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import GetAllBodyParts, Calculate1RM, CalculateWilks, CalculateDots, CalculateIpfGl, CalculateTotal,\
-    GetPublicExercises, GetPrivateExercises
+    GetPublicExercises, GetPrivateExercises, GetUserTrainingPlans, CreatePrivateExercise, DeletePrivateExercise
 
 urlpatterns = [
     path('1rm/', Calculate1RM.as_view(), name='calculate-1rm'),
@@ -9,6 +9,9 @@ urlpatterns = [
     path('ipfgl/', CalculateIpfGl.as_view(), name='calculate-ipf-gl'),
     path('total/', CalculateTotal.as_view(), name='calculate-total'),
     path('bodyparts/', GetAllBodyParts.as_view(), name='get-all-body-parts'),
+    path('createexercise/', CreatePrivateExercise.as_view(), name='create-exercise'),
     path('getpublicexercises/', GetPublicExercises.as_view(), name='get-public-exercises'),
     path('getprivateexercises/', GetPrivateExercises.as_view(), name='get-private-exercises'),
+    path('deleteexercise/', DeletePrivateExercise.as_view(), name='delete-exercise'),
+    path('getusertrainingplans/', GetUserTrainingPlans.as_view(), name='get-user-training-plans'),
 ]
