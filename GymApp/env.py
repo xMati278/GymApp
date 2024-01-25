9cf3a81bb3559b705a2e377b6257642de4b3d706
xMati278 <59803172+xMati278@users.noreply.github.com>
2024-01-25 20:41:00 +0100

    feat: update

--- GymApp/env.py
+++ GymApp/env.py
@@ -1,7 +1,11 @@
 import environ
 from django.core.management.utils import get_random_secret_key
 
+
+
 env = environ.Env(
     DEBUG=(bool, False),
-    SECRET_KEY=(str, get_random_secret_key())
+    SECRET_KEY=(str, get_random_secret_key()),
+    ENV=(str, 'dev'),
+    DB=(str, 'sqlite'),
 )
