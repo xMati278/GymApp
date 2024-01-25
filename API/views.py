c3f065d6f4b06e4c74adb1c9320258c62a691e35
xMati278 <59803172+xMati278@users.noreply.github.com>
2024-01-25 20:35:06 +0100

    feat(UserTrainingPlan): added CRUD for UserTrainingPlan

--- API/views.py
+++ API/views.py
@@ -1,12 +1,35 @@
 from rest_framework.viewsets import generics
 from django.http import JsonResponse
-from rest_framework.views import APIView
+from rest_framework.views import APIView, Response
 from .calculators import Calculators
 from .serializers import BodyPartSerializer, Calculate1RMSerializer, PointsCalculatorSerializer,\
-    CalculateTotalSerializer, CalculatorResultSerializer, TotalCalculatorResultSerializer, GetExercisesSerializer,\
-    GetUserTrainingPlansSerializer, CreatePrivateExerciseSerializer,\
-    DeleteExerciseSerializer
-from trainings.models import BodyPart, Exercise, UserTrainingPlans
+    CalculateTotalSerializer, CalculatorResultSerializer, TotalCalculatorResultSerializer, ExercisesSerializer,\
+    UserTrainingPlansSerializer, TrainingSerializer, TrainingRecordSerializer, TrainingPlanExerciseInfoSerializer,\
+    TrainingExerciseSerializer, RegistrationSerializer
+from trainings.models import BodyPart, Exercise, UserTrainingPlans, Training, TrainingRecord, TrainingPlanExerciseInfo,\
+    TrainingExercise
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework import status
+from django.contrib.auth.models import User
+
+class UserRegister(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegistrationSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username', None)
+
+        if User.objects.filter(username=username).exists():
+            return Response({'error': 'The user with the given login already exists.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class Calculate1RM(APIView):
@@ -169,17 +192,76 @@ class DeletePrivateExercise(generics.DestroyAPIView):
         except Exercise.DoesNotExist:
             self.permission_denied(self.request)
 
-class GetPublicExercises(generics.ListAPIView):
-    serializer_class = GetExercisesSerializer
-    queryset = Exercise.objects.filter(public=True)
+
+class CreateUserTrainingPlan(generics.CreateAPIView):
+    queryset = UserTrainingPlans.objects.all()
+    serializer_class = UserTrainingPlansSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def create(self, request, *args, **kwargs):
+        mutable_data = request.data.copy()
+        mutable_data['user'] = self.request.user.id
+
+        serializer = self.get_serializer(data=mutable_data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class GetPrivateExercises(generics.ListAPIView): #TODO walidacja
-
-    serializer_class = GetExercisesSerializer
+class ReadUserTrainingPlans(generics.ListAPIView):
+    queryset = UserTrainingPlans.objects.all()
+    serializer_class = UserTrainingPlansSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
 
     def get_queryset(self):
-        queryset = Exercise.objects.filter(user=self.request.user, public=False)
+        user = self.request.user
+        return UserTrainingPlans.objects.filter(user=user)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class UpdateUserTrainingPlan(generics.UpdateAPIView):
+    queryset = UserTrainingPlans.objects.all()
+    serializer_class = UserTrainingPlansSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def get_object(self) -> UserTrainingPlans:
+        user = self.request.user.id
+        plan_id = self.kwargs.get('pk')
+
+        try:
+            training_plan = UserTrainingPlans.objects.get(pk=plan_id, user=user)
+            return training_plan
+
+        except UserTrainingPlans.DoesNotExist:
+            self.permission_denied(self.request)
+
+
+class DeleteUserTrainingPlan(generics.DestroyAPIView):
+    queryset = UserTrainingPlans.objects.all()
+    serializer_class = UserTrainingPlansSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def get_object(self) -> UserTrainingPlans:
+        user = self.request.user.id
+        plan_id = self.kwargs.get('pk')
+
+        try:
+            training_plan = UserTrainingPlans.objects.get(pk=plan_id, user=user)
+            return training_plan
+        except UserTrainingPlans.DoesNotExist:
+            print('lol')
+            self.permission_denied(self.request)
 
         return queryset
 
