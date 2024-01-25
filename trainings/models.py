b6a276cfcc513ce8056ae9855fd91b74fd8c7312
xMati278 <59803172+xMati278@users.noreply.github.com>
2024-01-25 20:39:09 +0100

    feat(models): update models

--- trainings/models.py
+++ trainings/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class BodyPart(models.Model):
     name = models.CharField(max_length=30, help_text='name of the body part')
 
     def __str__(self):
-        return self.name
+        return f'{self.pk}. {self.name}'
 
 
 class Exercise(models.Model):
@@ -17,17 +17,23 @@ class Exercise(models.Model):
                              related_name='exercises', help_text='exercise owner')
 
     def __str__(self):
-        return self.name
+        return f'{self.pk}. {self.name} {("private: ",(self.user.username)) if self.public is False else "public" }'
 
+class TrainingPlanExerciseInfo(models.Model):
+    exercise = models.ForeignKey(Exercise,null=True, on_delete=models.CASCADE, related_name='exercise_id')
+    series = models.PositiveIntegerField(help_text="series number", validators=[MaxValueValidator(100),
+                                                                                MinValueValidator(1)])
+    reps = models.PositiveIntegerField(help_text="reps amount", validators=[MinValueValidator(1)])
 
-class UserTrainingPlans(models.Model):
+class UserTrainingPlans(models.Model): # TrainingPlan
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_training_plans', help_text='training plan owner')
     name = models.CharField(max_length=100, help_text='training plan name')
     last_training = models.DateTimeField(blank=True, null=True, help_text='date of last training')
-    exercises = models.ManyToManyField(Exercise, help_text='exercises assigned to this plan')
+    exercises = models.ManyToManyField(Exercise, blank=True,default=None, help_text='exercises assigned to this plan')
+    exercises_info = models.ManyToManyField(TrainingPlanExerciseInfo, blank=True) # [exercise: 1, resp:5, series: 5]
 
-    def __str__(self):
-        return f'{self.user.username} - {self.name} date: {self.last_training.date()}'
+    def __str__(self) -> str:
+        return f'{self.pk}. {self.user.username} - {self.name}'
 
 
 class Training(models.Model):
@@ -38,14 +44,14 @@ class Training(models.Model):
     training_plan = models.ForeignKey(UserTrainingPlans, on_delete=models.CASCADE, related_name='trainings', help_text='training plan')
     note = models.TextField(null=True, blank=True, default=None, help_text='optional training note')
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'{self.user.username} - {self.training_plan.name} time: {self.training_duration}'
 
 
 class Category(models.Model):
     category = models.CharField(max_length=50, help_text='training record category')
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.category
 
 
@@ -56,5 +62,23 @@ class TrainingRecord(models.Model):
                                  related_name='training_records', help_text='record category')
     value = models.FloatField(default=0, help_text='value of the record')
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'{self.user.username} - {self.exercise.name} - {self.category.category}: {self.value}'
+
+
+class TrainingExercise(models.Model):
+    training = models.ForeignKey(Training, on_delete=models.CASCADE, related_name='training_id',
+                                 help_text="exercise from this training")
+    exercise = models.ForeignKey(Exercise,default=None, on_delete=models.CASCADE, related_name='training_exercise',
+                                 help_text='record exercise')
+    series = models.PositiveIntegerField(help_text="series number", validators=[MaxValueValidator(100),
+                                                                                MinValueValidator(1)])
+    reps = models.PositiveIntegerField(help_text="reps amount", validators=[MinValueValidator(1)])
+    weight = models.FloatField(help_text="weight amount", validators=[MinValueValidator(1)])
+
+    def __str__(self) -> str:
+        return f'{self.training.user}, training: {self.training.id}, series: {self.series}, reps: {self.reps},' \
+               f' weight: {self.weight}'
+
+
+
