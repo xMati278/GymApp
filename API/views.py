0b1545c799391a34e5e43ac975e4bf6c8eda600b
xMati278 <59803172+xMati278@users.noreply.github.com>
2024-01-25 20:36:45 +0100

    feat(UserTrainingPlan): added CRUD for TrainingExercise

--- API/views.py
+++ API/views.py
@@ -446,3 +446,22 @@ class ReadTrainingExercise(generics.ListAPIView):
     serializer_class = TrainingExerciseSerializer
     permission_classes = [IsAuthenticated]
     authentication_classes = [JWTAuthentication]
+
+    def get_object(self):
+        exercise_info_id = self.kwargs.get('pk')
+        exercise_info = TrainingPlanExerciseInfo.objects.get(pk=exercise_info_id)
+
+        return exercise_info
+
+
+class UpdateTrainingExercise(generics.UpdateAPIView):
+    queryset = TrainingExercise.objects.all()
+    serializer_class = TrainingExerciseSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+class DestroyTrainingExercise(generics.DestroyAPIView):
+    queryset = TrainingExercise.objects.all()
+    serializer_class = TrainingExerciseSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
