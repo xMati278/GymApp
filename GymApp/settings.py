40c053439303e67de88a4c2c5c311b36d65cbfd0
xMati278 <59803172+xMati278@users.noreply.github.com>
2024-02-29 16:55:49 +0100

    fix(bandit): fixed RecursionError

--- GymApp/settings.py
+++ GymApp/settings.py
@@ -23,7 +23,7 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('MY_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
