652627422c7741e78488a71fdc863e53f5d62927
xMati278 <xmati278@gmail.com>
2024-08-24 11:52:44 +0200

    .

--- GymApp/settings.py
+++ GymApp/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 from .env import env
 import os
 from django.urls import reverse_lazy
-from django.conf.urls import handler404
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
