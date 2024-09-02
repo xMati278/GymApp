7b25c36537ae3565614d931d6111362c05585b19
xMati278 <xmati278@gmail.com>
2024-09-02 18:27:22 +0200

    changed admin panel

--- trainings/admin.py
+++ trainings/admin.py
@@ -3,8 +3,18 @@ from .models import BodyPart, Exercise, UserTrainingPlans, Training, Category, T
 # Register your models here.
 
 admin.site.register(BodyPart)
-admin.site.register(Exercise)
+
+
+
 admin.site.register(UserTrainingPlans)
 admin.site.register(Training)
 admin.site.register(TrainingRecord)
 admin.site.register(Category)
+
+
+class AdminExercise(admin.ModelAdmin):
+    list_display = [
+        'pk', 'name', 'user', 'public'
+    ]
+
+admin.site.register(Exercise, AdminExercise)
