496a6ef4fd7f7fcd1f19eab09537d8c17d2bb328
xMati278 <xmati278@gmail.com>
2024-09-27 17:09:11 +0200

    feat(api.user_training_plan): merge CRUD classes into consolidated views

--- trainings/API/views/user_training_plan.py
+++ trainings/API/views/user_training_plan.py
@@ -7,40 +7,6 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework import status
 
 
-# class CreateUserTrainingPlan(generics.CreateAPIView):
-#     queryset = UserTrainingPlans.objects.all()
-#     serializer_class = UserTrainingPlansSerializer
-#     permission_classes = [IsAuthenticated]
-#     authentication_classes = [JWTAuthentication]
-#
-#     def create(self, request, *args, **kwargs):
-#         mutable_data = request.data.copy()
-#         mutable_data['user'] = self.request.user.id
-#
-#         serializer = self.get_serializer(data=mutable_data)
-#         if serializer.is_valid(raise_exception=True):
-#             serializer.save()
-#             headers = self.get_success_headers(serializer.data)
-#             return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-#         else:
-#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#
-# class ReadUserTrainingPlans(generics.ListAPIView):
-#     queryset = UserTrainingPlans.objects.all()
-#     serializer_class = UserTrainingPlansSerializer
-#     permission_classes = [IsAuthenticated]
-#     authentication_classes = [JWTAuthentication]
-#
-#     def get_queryset(self):
-#         user = self.request.user
-#         return UserTrainingPlans.objects.filter(user=user)
-#
-#     def list(self, request, *args, **kwargs):
-#         queryset = self.get_queryset()
-#         serializer = self.get_serializer(queryset, many=True)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-
 class ListCreateUserTrainingPlanApiView(ListCreateAPIView):
     queryset = UserTrainingPlans.objects.all()
     serializer_class = UserTrainingPlansSerializer
