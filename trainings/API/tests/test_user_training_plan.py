77d5d106c571f33cd2c5ca1d484c3ce5b1af2667
xMati278 <xmati278@gmail.com>
2024-09-27 17:07:47 +0200

    feat(api.user_training_plan): merge CRUD classes into consolidated views

--- trainings/API/tests/test_user_training_plan.py
+++ trainings/API/tests/test_user_training_plan.py
@@ -16,7 +16,7 @@ class TestUserTrainingPlanApi(TestCase):
 
     def test_CreateUserTrainingPlan_returns_201_when_provided_correct_data(self):
         data = {'name': 'Extra GPP'}
-        response = self.client.post(reverse('create-user-training-plan'), data=data, content_type='application/json',
+        response = self.client.post(reverse('api-user-training-plan'), data=data, content_type='application/json',
                                     headers=self.auth_header)
         response_data = json.loads(response.content)
 
@@ -25,31 +25,31 @@ class TestUserTrainingPlanApi(TestCase):
 
     def test_CreateUserTrainingPlan_returns_400_when_not_provided_correct_data(self):
         data = {}
-        response = self.client.post(reverse('create-user-training-plan'), data=data, content_type='application/json',
+        response = self.client.post(reverse('api-user-training-plan'), data=data, content_type='application/json',
                                     headers=self.auth_header)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_CreateUserTrainingPlan_returns_401_when_authorization_token_not_provided(self):
         data = {'name': 'Extra GPP'}
-        response = self.client.post(reverse('create-user-training-plan'), data=data, content_type='application/json')
+        response = self.client.post(reverse('api-user-training-plan'), data=data, content_type='application/json')
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_ReadUserTrainingPlans_returns_200_when_provided_correct_data(self):
-        response = self.client.get(reverse('read-user-training-plans'), headers=self.auth_header)
+        response = self.client.get(reverse('api-user-training-plan'), headers=self.auth_header)
         response_data = json.loads(response.content)
 
         self.assertEqual(response_data[0]['name'], 'Test Plan')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_ReadUserTrainingPlans_returns_401_when_not_provided_authorization_token(self):
-        response = self.client.get(reverse('read-user-training-plans'))
+        response = self.client.get(reverse('api-user-training-plan'))
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
     def test_UpdateUserTrainingPlans_returns_200_when_provided_correct_data(self):
         data = {'name': "Push Test"}
-        response = self.client.patch(reverse('update-user-training-plan', kwargs={'pk': self.trainingplan.pk}), data=data,
+        response = self.client.patch(reverse('api-edit-user-training-plan', kwargs={'pk': self.trainingplan.pk}), data=data,
                                      headers=self.auth_header, content_type='application/json')
         response_data = json.loads(response.content)
 
@@ -58,19 +58,19 @@ class TestUserTrainingPlanApi(TestCase):
 
     def test_UpdateUserTrainingPlans_returns_401_when_authorization_token_not_provided(self):
         data = {'name': "Push Test"}
-        response = self.client.patch(reverse('update-user-training-plan', kwargs={'pk': 1}), data=data,
+        response = self.client.patch(reverse('api-edit-user-training-plan', kwargs={'pk': 1}), data=data,
                                      content_type='application/json')
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_DeleteUserTrainingPlan_returns_401_when_authorization_token_not_provided(self):
-        response = self.client.delete(reverse('delete-user-training-plan', kwargs={'pk': 1}),
+        response = self.client.delete(reverse('api-edit-user-training-plan', kwargs={'pk': 1}),
                                       content_type='application/json')
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_DeleteUserTrainingPlan_returns_204_when_authorization_token_provided(self):
-        response = self.client.delete(reverse('delete-user-training-plan', kwargs={'pk': self.trainingplan.pk}),
+        response = self.client.delete(reverse('api-edit-user-training-plan', kwargs={'pk': self.trainingplan.pk}),
                                       content_type='application/json', headers=self.auth_header)
 
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
--- trainings/API/urls.py
+++ trainings/API/urls.py
@@ -34,11 +34,13 @@ urlpatterns = [
     path('trainings/edit/<int:pk>', training.UpdateDestroyTrainingApiView.as_view(), name='api-trainings-edit'),
 
     #USER TRAINING PLAN
-    path('createusertrainingplan/', user_training_plan.CreateUserTrainingPlan.as_view(),
-         name='create-user-training-plan'),
-    path('readusertrainingplans/', user_training_plan.ReadUserTrainingPlans.as_view(), name='read-user-training-plans'),
-    path('updateusertrainingplan/<int:pk>', user_training_plan.UpdateUserTrainingPlan.as_view(),
-         name='update-user-training-plan'),
-    path('deleteusertrainingplan/<int:pk>', user_training_plan.DeleteUserTrainingPlan.as_view(),
-         name='delete-user-training-plan'),
+    path('user_training_plans/', user_training_plan.ListCreateUserTrainingPlanApiView.as_view(), name='api-user-training-plan'),
+    path('user_training_plans/edit/<int:pk>', user_training_plan.UpdateDestroyUserTrainingPlanApiView.as_view(), name='api-edit-user-training-plan'),
+    # path('createusertrainingplan/', user_training_plan.CreateUserTrainingPlan.as_view(),
+    #      name='create-user-training-plan'),
+    # path('readusertrainingplans/', user_training_plan.ReadUserTrainingPlans.as_view(), name='read-user-training-plans'),
+    # path('updateusertrainingplan/<int:pk>', user_training_plan.UpdateUserTrainingPlan.as_view(),
+    #      name='update-user-training-plan'),
+    # path('deleteusertrainingplan/<int:pk>', user_training_plan.DeleteUserTrainingPlan.as_view(),
+    #      name='delete-user-training-plan'),
 ]
