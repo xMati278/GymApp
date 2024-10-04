896673241a4f63ab09574deb35b806ab44cc0a52
xMati278 <xmati278@gmail.com>
2024-10-04 19:39:18 +0200

    docs(api.training_exercise): added docstring for api training_exercise crud

--- trainings/API/views/training_exercise.py
+++ trainings/API/views/training_exercise.py
@@ -6,12 +6,32 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.exceptions import NotFound
 
 class TrainingExerciseViewSet(ModelViewSet):
+    """
+    A viewset for managing training exercises. This allows authenticated users to retrieve, update,
+    and delete specific exercises associated with their training sessions.
+
+    **Permissions**:
+    - Only authenticated users can access and manage training exercises.
+
+    **Authentication**:
+    - JWT authentication is required to verify user identity.
+
+    **Functionality**:
+    - Users can manage their training exercises by retrieving, updating, or deleting them.
+    - Exercises are fetched based on the provided exercise ID.
+    """
+
     queryset = TrainingExercise.objects.all()
     serializer_class = TrainingExerciseSerializer
     permission_classes = [IsAuthenticated]
     authentication_classes = [JWTAuthentication]
 
     def get_object(self):
+        """
+        Retrieves a specific training exercise based on the provided exercise ID.
+        Raises a NotFound error if the exercise does not exist.
+        """
+
         exercise_info_id = self.kwargs.get('pk')
         try:
             exercise_info = TrainingExercise.objects.get(pk=exercise_info_id)
