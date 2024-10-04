eac3ff8cc2f1694792d2dae35fd5d9bc26a74055
xMati278 <xmati278@gmail.com>
2024-10-04 19:39:03 +0200

    docs(api.training): added docstring for api training crud

--- trainings/API/views/training.py
+++ trainings/API/views/training.py
@@ -8,16 +8,40 @@ from rest_framework.viewsets import ModelViewSet
 
 
 class TrainingViewSet(ModelViewSet):
+    """
+    A viewset for managing training sessions. This allows authenticated users to create, retrieve,
+    update, and delete their own training sessions.
+
+    **Permissions**:
+    - Only authenticated users can access and manage their own training data.
+
+    **Authentication**:
+    - JWT authentication is required to verify user identity.
+
+    **Functionality**:
+    - Users can only view and modify their own training sessions.
+    - The viewset handles creation, retrieval, and validation of training data.
+    """
+
     queryset = Training.objects.all()
     serializer_class = TrainingSerializer
     permission_classes = [IsAuthenticated]
     authentication_classes = [JWTAuthentication]
 
     def get_queryset(self):
+        """
+        Returns the training sessions specific to the authenticated user.
+        """
+
         user = self.request.user
         return Training.objects.filter(user=user)
 
     def get_object(self) -> Training:
+        """
+        Retrieves a specific training session object for the authenticated user.
+        Raises a permission error if the session does not exist or does not belong to the user.
+        """
+
         user = self.request.user.id
         training_id = self.kwargs.get('pk')
 
@@ -28,6 +52,11 @@ class TrainingViewSet(ModelViewSet):
             self.permission_denied(self.request)
 
     def create(self, request, *args, **kwargs):
+        """
+        Creates a new training session for the authenticated user.
+        The user is automatically associated with the training session.
+        """
+
         mutable_data = request.data.copy()
         mutable_data['user'] = self.request.user.id
 
